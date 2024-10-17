14e6bf2a83d225c204ce5ea5e14ccf73ec52af2a
mvfflin <mynameisfatihul@gmail.com>
2024-10-17 22:49:43 +0700

    feat: update isi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,19 @@ export default function Home() {
     return (
         <div style={{ backgroundColor: "white" }}>
             <Fullpage
+                controlArrows={true}
+                // controlArrowsHTML={[
+                //     '<div class="my-arrow"><i class="fas fa-arrow-alt-circle-left">',
+                //     '<div class="my-arrow"><i class="fas fa-arrow-alt-circle-right">',
+                // ]}
+                css3={false}
+                // scrollHorizontally={true}
                 credits={{
                     enabled: false,
                     label: "",
                     position: "right",
                 }}
-                navigation={true}
+                navigation={false}
                 // sectionsColor={["white"]}
                 navigationTooltips={["Judul", "Sejarah"]}
                 render={({ state, fullpageApi }) => {
@@ -44,120 +51,54 @@ export default function Home() {
                                 </div>
                             </div>
                             <div className="section">
-                                <div className="grid grid-2 grid-flow-col w-full h-screen">
-                                    <div className="bg-zinc-900 h-full w-full justify-center p-5 pr-14 md:pr-5 content-center">
-                                        <h1 className="text-5xl text-white font-helvetica font-bold">
-                                            Sejarah Kota Bekasi
-                                        </h1>
-                                        <hr className="my-5 border-white border-2" />
-                                        <h1 className="text-zinc-300 text-justify">
-                                            Lorem ipsum dolor sit amet
-                                            consectetur adipisicing elit.
-                                            Mollitia, a amet labore repudiandae
-                                            asperiores maiores vitae consequatur
-                                            aspernatur temporibus dolore minus
-                                            officia praesentium illo ad ipsa
-                                            enim quasi at veritatis maxime
-                                            necessitatibus obcaecati
-                                            perspiciatis sunt. Fugit deserunt
-                                            officia, laudantium fugiat tenetur
-                                        </h1>
-                                    </div>
-                                    <div className="bg-orange-100 hidden md:flex">
-                                        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 p-3 h-full">
-                                            <div className="grid gap-4">
-                                                <div>
-                                                    <img
-                                                        className="h-auto max-w-full rounded-lg"
-                                                        src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg"
-                                                        alt=""
-                                                    />
-                                                </div>
-                                                <div>
-                                                    <img
-                                                        className="h-auto max-w-full rounded-lg"
-                                                        src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg"
-                                                        alt=""
-                                                    />
-                                                </div>
-                                                <div>
-                                                    <img
-                                                        className="h-auto max-w-full rounded-lg"
-                                                        src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg"
-                                                        alt=""
-                                                    />
-                                                </div>
-                                            </div>
-                                            <div className="grid gap-4">
-                                                <div>
-                                                    <img
-                                                        className="h-auto max-w-full rounded-lg"
-                                                        src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg"
-                                                        alt=""
-                                                    />
-                                                </div>
-                                                <div>
-                                                    <img
-                                                        className="h-auto max-w-full rounded-lg"
-                                                        src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg"
-                                                        alt=""
-                                                    />
-                                                </div>
-                                                <div>
-                                                    <img
-                                                        className="h-auto max-w-full rounded-lg"
-                                                        src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg"
-                                                        alt=""
-                                                    />
-                                                </div>
-                                            </div>
-                                            <div className="grid gap-4">
-                                                <div>
-                                                    <img
-                                                        className="h-auto max-w-full rounded-lg"
-                                                        src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg"
-                                                        alt=""
-                                                    />
-                                                </div>
-                                                <div>
-                                                    <img
-                                                        className="h-auto max-w-full rounded-lg"
-                                                        src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg"
-                                                        alt=""
-                                                    />
-                                                </div>
-                                                <div>
-                                                    <img
-                                                        className="h-auto max-w-full rounded-lg"
-                                                        src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg"
-                                                        alt=""
-                                                    />
-                                                </div>
-                                            </div>
-                                            <div className="grid gap-4">
-                                                <div>
-                                                    <img
-                                                        className="h-auto max-w-full rounded-lg"
-                                                        src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg"
-                                                        alt=""
-                                                    />
-                                                </div>
-                                                <div>
-                                                    <img
-                                                        className="h-auto max-w-full rounded-lg"
-                                                        src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg"
-                                                        alt=""
-                                                    />
-                                                </div>
-                                                <div>
-                                                    <img
-                                                        className="h-auto max-w-full rounded-lg"
-                                                        src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg"
-                                                        alt=""
-                                                    />
-                                                </div>
-                                            </div>
+                                <div className="slide">
+                                    <div className="grid grid-2 grid-flow-col w-full h-screen">
+                                        <div className="bg-zinc-900 h-full w-full justify-center p-5 pr-14 md:pr-5 content-center">
+                                            <h1 className="text-7xl text-white font-helvetica font-bold text-center">
+                                                Sejarah Kota Bekasi
+                                            </h1>
+                                            <h1 className="text-4xl text-white mt-3 font-helvetica font-medium text-center">
+                                                Scroll ke kanan!
+                                            </h1>
                                         </div>
+                                        <div className="bg-orange-100 hidden md:flex">
+                                            <img
+                                                className="h-auto max-w-full rounded-lg"
+                                                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg"
+                                                alt=""
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="slide bg-oldbekasi p-20">
+                                    <div className="h-full w-full justify-center p-5 pr-14 md:pr-5 content-center">
+                                        <h1 className="text-3xl text-white font-poppins leading-relaxed text-center">
+                                            Sejarah Tahun 1949 Sampai
+                                            Terbentuknya Kota Bekasi Sejarah
+                                            setelah tahun 1949 ditandai aksi
+                                            unjuk rasa sekitar 40.000 rakyat
+                                            Bekasi pada tanggal 17 Februari 1950
+                                            di alun-alun Bekasi. Hadir pada
+                                            acara terseut Bapak Mu’min sebagai
+                                            Resuden Militer Daerah V. Inti dari
+                                            unjuk rasa tersebut adalah
+                                            penyapaian pernyataan sikap sebagai
+                                            berikut. Rakyat Bekasi mengajukan
+                                            usul kepada pemeritah pusat agar
+                                            Kabupaten Jatinegara diubah menjadi
+                                            Kabupaten Bekasi. Rakyat Bekasi
+                                            tetap berdiri di belakang pemerintah
+                                            Negara Kesatuan Republik Indonesia.
+                                            Dan berdasarkan Undang-undang No 14
+                                            tahun 1950 terbentuklah Kabupaten
+                                            Bekasi, dengan wilayah terdiri dari
+                                            empat kewedanan, 13 kecamatan,
+                                            (termasuk Kecamatan Cibarusah) dan
+                                            95 desa. Angka-angka itu sevara
+                                            simbolis diungkapkan dalam lambang
+                                            Kabupaten Bekasi dengan motto
+                                            “Swantara Wibawa Mukti”,
+                                        </h1>
                                     </div>
                                 </div>
                             </div>
